dabddff5c3366d8455542cf168ac2fadc1ad3d70
joseph townshend <joetownshend@mac.com>
2019-11-01 13:05:31 +0000

    feat: hacker news h1 added

---
+++ src/styles/StoryStyles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StoryWrapper = styled.section`
+  padding-top: 10px;
+  margin-bottom: 20px;
+  border-top: 1px solid #ccc;
+
+  &:first-of-type {
+    border-top: 0;
+  }
+
+  &:last-of-type {
+    margin-bottom: 0px;
+    padding-bottom: 0px;
+  }
+
+`;`
