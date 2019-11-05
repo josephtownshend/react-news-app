18046fab679f967cf67e7657b1902344eaa8d695
joseph townshend <joetownshend@mac.com>
2019-11-05 16:35:23 +0000

    bug: global styles not importing

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getStoryIds } from '../services/hnApi';
 import { Story } from '../components/Story';
+import { GlobalStyle, StoriesContainerWrapper } from '../styles/StoriesContainerStyles';
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
@@ -11,10 +12,13 @@ export const StoriesContainer = () => {
 
   return (
     <>
+    <GlobalStyle />
+    <StoriesContainerWrapper data-testid="stories-container">
       <h1>Hacker News Stories</h1>
       {storyIds.map(storyId => (
         <Story key={storyId} storyId={storyId} />
       ))}
+      </StoriesContainerWrapper>
     </>
   );
 };
